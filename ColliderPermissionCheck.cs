79e956fa9db41477cbb809bb9517faebe4265569
aussieguy92 <80846814+aussieguy92@users.noreply.github.com>
2023-09-14 18:58:57 +0930

    Main Commit

---
+++ ColliderPermissionCheck.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * ColliderPermissionChecks
+ * Author: AussieGuy92
+ * License: Open Source (MIT License)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ColliderPermissionCheck : UdonSharpBehaviour
+{
+    public Collider restrictedCollider;
+    public Transform teleportLocation;
+    public VRCPlayerApi[] playersWithPermission = new VRCPlayerApi[50]; // Assuming a max of 50 players with permission
+    private VRCPlayerApi[] allPlayers = new VRCPlayerApi[80]; // Assuming a max of 80 players in the instance
+
+    private void Start()
+    {
+        // Add the local player to the array when the script starts
+        AddPlayerToArray(Networking.LocalPlayer, allPlayers);
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        AddPlayerToArray(player, allPlayers);
+    }
+
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        RemovePlayerFromArray(player, allPlayers);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        VRCPlayerApi player = other.GetComponent<VRCPlayerApi>();
+        if (player == null) return;
+
+        if (!HasPermission(player))
+        {
+            player.TeleportTo(teleportLocation.position, teleportLocation.rotation);
+        }
+    }
+
+    private void Update()
+    {
+        // Check all players inside the collider every frame
+        foreach (VRCPlayerApi player in allPlayers)
+        {
+            if (player == null) continue;
+            if (restrictedCollider.bounds.Contains(player.GetPosition()) && !HasPermission(player))
+            {
+                player.TeleportTo(teleportLocation.position, teleportLocation.rotation);
+            }
+        }
+    }
+
+    public void GrantPermission(VRCPlayerApi player)
+    {
+        if (!HasPermission(player))
+        {
+            AddPlayerToArray(player, playersWithPermission);
+        }
+    }
+
+    private bool HasPermission(VRCPlayerApi player)
+    {
+        foreach (VRCPlayerApi permittedPlayer in playersWithPermission)
+        {
+            if (permittedPlayer == player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void AddPlayerToArray(VRCPlayerApi player, VRCPlayerApi[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == null)
+            {
+                array[i] = player;
+                break;
+            }
+        }
+    }
+
+    private void RemovePlayerFromArray(VRCPlayerApi player, VRCPlayerApi[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == player)
+            {
+                array[i] = null;
+                break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# VRC Area Protection
- Udon C# Area protection script for VRChat.
+Overview
+The provided scripts are designed to manage player permissions within specific colliders in VRChat. The primary goal is to restrict unauthorized players from entering a designated area and provide an interactive mechanism (a button) for players to gain access.
+
+Scripts Breakdown
+1. ColliderPermissionCheck
+This script is responsible for managing player permissions and ensuring unauthorized players are teleported out of a restricted area.
+
+Key Variables:
+restrictedCollider: The collider that represents the restricted area.
+teleportLocation: The location where unauthorized players will be teleported to.
+playersWithPermission: An array of players who have been granted permission to enter the restricted area.
+Methods:
+OnTriggerEnter(Collider other): Triggered when a player enters the restricted collider. If the player doesn't have permission, they are teleported to the designated location.
+
+Update(): Continuously checks if any player is inside the restricted collider. If a player without permission is found inside, they are teleported out. This ensures that even players who somehow bypass the OnTriggerEnter (e.g., by teleporting directly inside) are also handled.
+
+GrantPermission(VRCPlayerApi player): Grants a player permission to enter the restricted area. The player is added to the playersWithPermission array.
+
+HasPermission(VRCPlayerApi player): Checks if a player is in the playersWithPermission array, i.e., if they have permission.
+
+2. BoxButtonPermission
+This script turns a GameObject (like a cube) into an interactive button. When players interact with this button, they are granted permission to enter the restricted area.
+
+Key Variables:
+permissionManager: A reference to the ColliderPermissionCheck script, which manages player permissions.
+Methods:
+Interact(): Triggered when a player interacts with the GameObject. The player is granted permission to enter the restricted area by calling the GrantPermission method of the permissionManager.
+Workflow:
+A player approaches the restricted area.
+If they enter the restricted collider without permission, they are immediately teleported out.
+The player interacts with the designated button (e.g., a cube with the BoxButtonPermission script).
+The button grants the player permission by adding them to the playersWithPermission array in the ColliderPermissionCheck script.
+Now, the player can enter the restricted area without being teleported out.
